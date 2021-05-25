382a18e33958192da9aed0c824c0c41302327563
aliaksei6777 <aliaksei6777@gmail.com>
2021-05-25 21:50:33 +0300

    gh pages

--- src/n2-features/f0-test/TestPage.tsx
+++ src/n2-features/f0-test/TestPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components"
+import { StyledButton } from "../../n1-main/m1-ui/u1-common/styles/Button";
 
 const TestPageContainer = styled.div`
   display: flex;
@@ -10,6 +11,7 @@ export const TestPage = () => {
     return (
         <TestPageContainer>
                 <h3>Test Page</h3>
+            <StyledButton>button</StyledButton>
         </TestPageContainer>
     )
 }
