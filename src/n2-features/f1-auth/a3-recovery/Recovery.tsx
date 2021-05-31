029ec8923c818ae7fe9af9b109f886eacf5f9a27
tihon <tihonnet@gmail.com>
2021-05-31 23:47:17 +0300

    feat: add state

--- src/n2-features/f1-auth/a3-recovery/Recovery.tsx
+++ src/n2-features/f1-auth/a3-recovery/Recovery.tsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, {ChangeEvent, useState} from "react";
 import styled from "styled-components"
 
 const RecoveryContainer = styled.div`
-      display: flex;
-      justify-content: center;
-    `
+  display: flex;
+  justify-content: center;
+`
 
 export const Recovery = () => {
+    let [email, setEmail] = useState<string>('')
+
+    let onChangeEmail = (event: ChangeEvent<HTMLInputElement>) => {
+        let date = event.currentTarget.value
+        setEmail(date)
+    }
+
     return (
-        <RecoveryContainer>
-            <h3>Recovery page</h3>
-        </RecoveryContainer>
+        <>
+            <RecoveryContainer>
+                <h3>Recovery page</h3>
+            </RecoveryContainer>
+            <div>
+                <input value={email} onChange={onChangeEmail}/>
+                <button onClick={() => alert(email)}> send</button>
+            </div>
+        </>
     )
 }
