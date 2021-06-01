bf5841be78328e3b7956e9a351b4365d1da8ca21
Alex Kokhan <kokhan.a.a@yandex.by>
2021-06-01 10:54:48 +0300

    Made and connected thunk

--- src/n2-features/f1-auth/a2-register/r2 - bll/registrationReducer.ts
+++ src/n2-features/f1-auth/a2-register/r2 - bll/registrationReducer.ts
@@ -1,12 +1,18 @@
 import {Dispatch} from "redux";
+import { setErrorRegistrationActionType, setSuccessRegistrationActionType } from "./actionRegistrationReducer";
 
+//type
+export type InitialStateType = {
+    error: string | null
+    success: boolean
+}
 
+export type ActionsType = setErrorRegistrationActionType | setSuccessRegistrationActionType
 
 export const initialState = {
     error: null,
     success: false
 }
-
 
 export const registrationReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
     switch (action.type) {
@@ -22,22 +28,3 @@ export const registrationReducer = (state: InitialStateType = initialState, acti
 }
 
 
-export const setErrorRegistration = (error: string | null) => {
-    return {type: 'registration/SET-ERROR-REGISTRATION', error} as const
-}
-
-export const setSuccessRegistration = (success: boolean) => {
-    return {type: 'registration/SET-SUCCESS-REGISTRATION', success} as const
-}
-
-
-
-//type
-export type InitialStateType = {
-    error: string | null
-    success: boolean
-}
-type setErrorRegistrationActionType = ReturnType<typeof setErrorRegistration>
-type setSuccessRegistrationActionType = ReturnType<typeof setSuccessRegistration>
-
-export type ActionsType = setErrorRegistrationActionType | setSuccessRegistrationActionType
