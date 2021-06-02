77d183b3a3117be9791641979cf378ddb58c6c5c
aliaksei6777 <aliaksei6777@gmail.com>
2021-06-02 23:51:44 +0300

    change baseURL to https://neko-back.herokuapp.com/2.0

--- src/n1-main/m3-dal/api.ts
+++ src/n1-main/m3-dal/api.ts
@@ -3,8 +3,8 @@ import {UserDataType} from "../../n2-features/f1-auth/a1-login/auth-reducer";
 
 export const instance = axios.create({
     withCredentials: true,
-    baseURL: 'http://localhost:7542/2.0/',
-    // baseURL: 'https://neko-back.herokuapp.com/2.0',
+    // baseURL: 'http://localhost:7542/2.0/',
+    baseURL: 'https://neko-back.herokuapp.com/2.0',
 })
 
 export type APIResponseType<D = {}> = {
